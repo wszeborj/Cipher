f91038572ec260b749f7d371823cdd8f8d68d6b8
wszeborj <wszeborj@o2.pl>
2023-03-12 22:35:36 +0100

    feat: added new class manager

--- menu_manager.py
+++ menu_manager.py
@@ -1,3 +1,5 @@
+import sys
+
 class Menu:
     def show(self):
         print('1. Szyfruj plik metodą rot13')
@@ -8,10 +10,41 @@ class Menu:
 
     def get_choice(self):
         try:
-            return int(input('Wybierz opcję: '))
+            choice = int(input('Wybierz opcję: '))
+            if choice < 1 or 5 < choice:
+                raise ValueError
         except ValueError:
             print('Błędna opcja! Podaj numer z zakresu 1 - 5.')
+            self.get_choice()
 
 
+class Manager:
+    def __init__(self):
+        self.menu = Menu()
+
+    def start(self):
+        self.menu.show()
+        self.execute()
+
+    def execute(self):
+        choice = self.menu.get_choice()
+
+        if choice == 1:
+            pass
+        elif choice == 2:
+            pass
+        elif choice == 3:
+            pass
+        elif choice == 4:
+            pass
+        elif choice == 5:
+            sys.exit()
 
 
+def main():
+    m = Manager()
+    m.start()
+
+
+if __name__ == '__main__':
+    main()

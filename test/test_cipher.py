2dd0dce4996919c98480adaf5004939579ebbeda
wszeborj <wszeborj@o2.pl>
2023-03-27 22:20:35 +0200

    test: created tests for cipher.py

---
+++ test/test_cipher.py
@@ -0,0 +1,10 @@
+from functionality.cipher import cipher
+import pytest
+
+
+def test_return_ciphered_rot13_string_after_providing_string_with_lower_upper_letters_number_signs():
+    assert cipher(input_text='zupa1, ZUPA1.!?', shift=13) == 'mhcn4, MHCN4.!?'
+
+
+def test_return_ciphered_rot47_string_after_providing_string_with_lower_upper_letters_number_signs():
+    assert cipher(input_text='zupa1, ZUPA1.!?', shift=47) == 'upkv8, UPKV8.!?'

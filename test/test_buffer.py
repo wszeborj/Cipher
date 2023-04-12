c8791916bf30c7d5ebf98e690bc6a5f76032bd2f
wszeborj <wszeborj@o2.pl>
2023-04-12 22:39:49 +0200

    test: new test added to test_buffer and test_cipher somoething wrong with status in Text

--- test/test_buffer.py
+++ test/test_buffer.py
@@ -1,15 +1,60 @@
+import unittest
+from dataclasses import asdict
+from unittest.mock import Mock, patch
+
 from functionality.buffer import Buffer, Text
 
 
-def test_add_obj_text_to_buffer_return_added_obj_buffer():
-    buffer = Buffer()
-    buffer.add(Text(text="Zupa1", rot_type=13, status="encrypted"))
-    assert buffer.data == [{"text": "Zupa1", "status": "encrypted", "rot_type": 13}]
+class TestText(unittest.TestCase):
+    def test_set_status_to_encrypted_when_true_status_was_provided(self):
+        provided = Text(text="", rot_type=13, status=True)
+        expected = Text(text="", rot_type=13, status="encrypted")
+        assert provided == expected
+
+    def test_set_status_to_decrypted_when_false_status_was_provided(self):
+        provided = Text(text="", rot_type=13, status=False)
+        expected = Text(text="", rot_type=13, status="decrypted")
+        assert provided == expected
+
+    def test_set_status_to_decrypted_when_string_status_was_provided(self):
+        provided = Text(text="", rot_type=13, status="False")
+        expected = Text(text="", rot_type=13, status="encrypted")
+        assert provided == expected
 
 
-def test_extend_list_of_dict_to_buffer_return_extended_list():
-    pass
+class TestBuffer(unittest.TestCase):
+    def setUp(self):
+        self.buffer = Buffer()
+        self.text1 = Text(text="Ala ma kota", rot_type=13, status=False)
+        self.text2 = Text(text="zupa1.", rot_type=47, status=True)
+        self.dict1 = asdict(self.text1)
+        self.dict2 = asdict(self.text2)
 
+    def test_add_obj_text_to_buffer_return_list_of_dict(self):
+        self.buffer.add(self.text1)
+        assert self.buffer.data == [self.dict1]
+        self.buffer.add(self.text2)
+        assert self.buffer.data == [self.dict1, self.dict2]
 
-def test_clear_list_buffer_return_empty_list():
-    pass
+    def test_extend_list_of_dict_to_buffer_return_extended_list(self):
+        self.buffer.extend([self.dict1, self.dict2])
+        assert self.buffer.data == [self.dict1, self.dict2]
+
+    def test_clear_list_buffer_return_empty_list(self):
+        self.buffer.extend([self.dict1, self.dict2])
+        self.buffer.clear()
+        assert self.buffer.data == []
+
+    def test_show_all_buffer_data(self):
+        self.buffer.add(self.text1)
+        self.buffer.add(self.text2)
+        with unittest.mock.patch("builtins.print") as mock_print:
+            self.buffer.show_all()
+            mock_print.assert_called_with([self.dict1, self.dict2])
+
+    @patch("builtins.print")
+    def test_show_all_buffer_data_2(self, mock_print):
+        self.buffer.add(self.text1)
+        self.buffer.add(self.text2)
+        self.buffer.show_all()
+        mock_print.assert_called_with([self.dict1, self.dict2])
--- test/test_cipher.py
+++ test/test_cipher.py
@@ -7,32 +7,33 @@ import pytest
 class TestCipher:
     def setup_method(self):
         self.word = "zupa1, ZUPA1.!?"
-        self.word_1 = "mhcn4, MHCN4.!?"
+        self.word_13 = "mhcn4, MHCN4.!?"
+        self.word_47 = "upkv8, UPKV8.!?"
 
-    def test_return_ciphered_rot13_string_after_providing_string_with_lower_upper_letters_number_signs(
+    def test_return_encrypted_rot13_string_after_providing_string_with_lower_upper_letters_number_signs(
         self,
     ):
         assert CaesarCipher.encrypt_decrypt(
-            input_text="zupa1, ZUPA1.!?", shift=13, crypting=True
-        ) == Text(text="mhcn4, MHCN4.!?", status="decrypted", rot_type=13)
+            input_text=self.word, shift=13, crypting=True
+        ) == Text(text=self.word_13, status="encrypted", rot_type=13)
 
-    def test_return_unciphered_rot13_string_after_providing_string_with_lower_upper_letters_number_signs(
+    def test_return_decrypted_rot13_string_after_providing_string_with_lower_upper_letters_number_signs(
         self,
     ):
         assert CaesarCipher.encrypt_decrypt(
-            input_text="mhcn4, MHCN4.!?", shift=13, crypting=False
-        ) == Text(text="zupa1, ZUPA1.!?", status="decrypted", rot_type=13)
+            input_text=self.word_13, shift=13, crypting=False
+        ) == Text(text=self.word, status="decrypted", rot_type=13)
 
-    def test_return_ciphered_rot47_string_after_providing_string_with_lower_upper_letters_number_signs(
+    def test_return_encrypted_rot47_string_after_providing_string_with_lower_upper_letters_number_signs(
         self,
     ):
         assert CaesarCipher.encrypt_decrypt(
-            input_text="zupa1, ZUPA1.!?", shift=47, crypting=True
-        ) == Text(text="upkv8, UPKV8.!?", status="encrypted", rot_type=47)
+            input_text=self.word, shift=47, crypting=True
+        ) == Text(text=self.word_47, status="encrypted", rot_type=47)
 
-    def test_return_unciphered_rot47_string_after_providing_string_with_lower_upper_letters_number_signs(
+    def test_return_decrypted_rot47_string_after_providing_string_with_lower_upper_letters_number_signs(
         self,
     ):
         assert CaesarCipher.encrypt_decrypt(
-            input_text="upkv8, UPKV8.!?", shift=47, crypting=False
-        ) == Text(text="zupa1, ZUPA1.!?", status="decrypted", rot_type=47)
+            input_text=self.word_47, shift=47, crypting=False
+        ) == Text(text=self.word, status="decrypted", rot_type=47)

78ef10bf3dd7ab83acbad39f66c5c7f8089b5762
wszeborj <wszeborj@o2.pl>
2023-04-19 22:10:39 +0200

    test: fixed tests of file_handling

--- test/test_file_handling.py
+++ test/test_file_handling.py
@@ -2,8 +2,13 @@ import tempfile
 import unittest
 import json
 import os
+import pytest
 from unittest.mock import patch
 from functionality.file_handling import FileHandler
+
+
+# def test_xyz():
+# with pytest.raises(json.decoder.JSONDecodeError):
 
 
 class TestFileHandler(unittest.TestCase):
@@ -12,9 +17,6 @@ class TestFileHandler(unittest.TestCase):
         self.file_path = r"C:\Users\user\OneDrive\Dokumenty\cipher\test\test.json"
         self.data1 = [{"text": "mhcn4", "status": "encrypted", "rot_type": 13}]
         self.data2 = [{"text": "mhcn5", "status": "encrypted", "rot_type": 13}]
-        self.invalid_file_path = (
-            r"C:\Users\user\OneDrive\Dokumenty\cipher\test\test2.json"
-        )
 
     # def test_open_file(self):
     #     with tempfile.NamedTemporaryFile(mode="w", delete=False) as temp_file:
@@ -30,6 +32,7 @@ class TestFileHandler(unittest.TestCase):
     def test_provide_path_to_json_file_to_open_file_return_list_dictionaries(self):
         with open(self.file_path, "w") as json_file:
             json.dump(self.data1, json_file)
+
         loaded_data = FileHandler.open_file(self.file_path)
         self.assertEquals(loaded_data, self.data1)
         os.remove(self.file_path)
@@ -38,8 +41,12 @@ class TestFileHandler(unittest.TestCase):
         with open(self.file_path, "w") as inv_json_file:
             inv_json_file.write("test data")
 
-        loaded_inv_data = FileHandler.open_file(self.file_path)
-        self.assertEquals(loaded_inv_data, [])
+        with unittest.mock.patch("builtins.print") as mock_print:
+            loaded_inv_data = FileHandler.open_file(self.file_path)
+            mock_print.assert_called_with(
+                f"Expecting value: line 1 column 1 (char 0), File is not json file"
+            )
+            self.assertEquals(loaded_inv_data, [])
         os.remove(self.file_path)
 
     def test_provide_data_for_save_file_and_write_file_with_saved_data(self):
@@ -55,7 +62,7 @@ class TestFileHandler(unittest.TestCase):
 
     def test_provide_data_for_save_file_and_append_file_with_saved_data(self):
         operation = "a"
-        expected_data = self.data1.extend(self.data2)
+        expected_data = self.data2 + self.data1
 
         with open(self.file_path, "w") as json_file:
             json.dump(self.data1, json_file)
@@ -65,7 +72,5 @@ class TestFileHandler(unittest.TestCase):
         )
         with open(self.file_path, "r") as json_file:
             saved_data = json.load(json_file)
-        print(f"{saved_data=}")
-        print(f"{expected_data=}")
         self.assertEquals(saved_data, expected_data)
         os.remove(self.file_path)

aa06c65bdc13ef76e20e132f58b964dfc07cffce
wszeborj <wszeborj@o2.pl>
2023-04-22 22:13:09 +0200

    fix: changed status in tests changed assert to self.assertEquals

--- test/test_cipher.py
+++ test/test_cipher.py
@@ -1,10 +1,10 @@
-from functionality.buffer import Text, Buffer
+from functionality.buffer import Text
 from functionality.cipher import CaesarCipher
-
+import unittest
 import pytest
 
 
-class TestCipher:
+class TestCipher(unittest.TestCase):
     def setup_method(self):
         self.word = "zupa1, ZUPA1.!?"
         self.word_13 = "mhcn4, MHCN4.!?"
@@ -13,27 +13,35 @@ class TestCipher:
     def test_return_encrypted_rot13_string_after_providing_string_with_lower_upper_letters_number_signs(
         self,
     ):
-        assert CaesarCipher.encrypt_decrypt(
-            input_text=self.word, shift=13, crypting=True
-        ) == Text(text=self.word_13, status="encrypted", rot_type=13)
+        self.assertEquals(
+            CaesarCipher.encrypt_decrypt(input_text=self.word, shift=13, crypting=True),
+            Text(text=self.word_13, status=True, rot_type=13),
+        )
 
     def test_return_decrypted_rot13_string_after_providing_string_with_lower_upper_letters_number_signs(
         self,
     ):
-        assert CaesarCipher.encrypt_decrypt(
-            input_text=self.word_13, shift=13, crypting=False
-        ) == Text(text=self.word, status="decrypted", rot_type=13)
+        self.assertEquals(
+            CaesarCipher.encrypt_decrypt(
+                input_text=self.word_13, shift=13, crypting=False
+            ),
+            Text(text=self.word, status=False, rot_type=13),
+        )
 
     def test_return_encrypted_rot47_string_after_providing_string_with_lower_upper_letters_number_signs(
         self,
     ):
-        assert CaesarCipher.encrypt_decrypt(
-            input_text=self.word, shift=47, crypting=True
-        ) == Text(text=self.word_47, status="encrypted", rot_type=47)
+        self.assertEquals(
+            CaesarCipher.encrypt_decrypt(input_text=self.word, shift=47, crypting=True),
+            Text(text=self.word_47, status=True, rot_type=47),
+        )
 
     def test_return_decrypted_rot47_string_after_providing_string_with_lower_upper_letters_number_signs(
         self,
     ):
-        assert CaesarCipher.encrypt_decrypt(
-            input_text=self.word_47, shift=47, crypting=False
-        ) == Text(text=self.word, status="decrypted", rot_type=47)
+        self.assertEquals(
+            CaesarCipher.encrypt_decrypt(
+                input_text=self.word_47, shift=47, crypting=False
+            ),
+            Text(text=self.word, status=False, rot_type=47),
+        )
